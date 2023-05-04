51f169ea73a581d291e6a1a1c0a40fb8bc280821
Dikendev <diken.dev@gmail.com>
2023-05-03 22:00:21 -0300

    Product Controller initial

---
+++ src/main/java/com/diken/productservice/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.diken.productservice.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+}
